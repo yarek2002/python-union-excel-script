e1564c45de1aa6932888c980defeaf48f690b5a9
yarek2002 <yarsen2002@gmail.com>
2025-11-28 13:47:50 +0300

    fix erorrr

--- union.py
+++ union.py
@@ -73,17 +73,21 @@ def extract_file_data(file_path):
     record = defaultdict(lambda: pd.NA)
     record["Файл"] = os.path.basename(file_path)
 
+    # если body пустой — ничего не делаем (или возвращаем запись)
+    if body.empty:
+        return record  # <- в extract_file_data: вернём пустую запись
+
     stop = None
     for i in range(len(body)):
         val = body.iloc[i, 0]
 
-    # если NaN, пусто или не число → конец 1 секции
         if pd.isna(val) or str(val).strip() == "" or not is_numeric(val):
             stop = i
             break
 
     if stop is not None:
         body = body.iloc[:stop]
+
 
 
     # Документ (номер или название)
